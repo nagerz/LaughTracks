7fcdd6b78a52eef6cb328684bd77cf7ff280479b
nagerz <znager@gmail.com>
2019-01-23 22:18:58 -0700

    Initial commit + 1 controller method

--- app/controllers/laugh_tracks_app.rb
+++ app/controllers/laugh_tracks_app.rb
@@ -1,2 +1,14 @@
 class LaughTracksApp < Sinatra::Base
+
+  get '/comedians' do
+    #@comedians = Comedian.find_by(params[:filter])
+    @comedians = Comedian.all
+    erb :"comedians/index"
+  end
+
+  # get '/comedians/new' do
+  #   erb :"comedians/new"
+  # end
+
+
 end
