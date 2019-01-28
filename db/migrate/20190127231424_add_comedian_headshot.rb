b41ba2f9df0f86be975dae817f24175f1cfcefce
nagerz <znager@gmail.com>
2019-01-27 17:13:42 -0700

    Added comedian headshot. Added styling pre-bootstrap.

---
+++ db/migrate/20190127231424_add_comedian_headshot.rb
@@ -0,0 +1,5 @@
+class AddComedianHeadshot < ActiveRecord::Migration[5.2]
+  def change
+    add_column(:comedians, :headshot, :string)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_25_193133) do
+ActiveRecord::Schema.define(version: 2019_01_27_231424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_01_25_193133) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "headshot"
   end
 
   create_table "specials", force: :cascade do |t|
